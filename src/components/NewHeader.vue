a4bdfa6d015ce4d407190dd1fe3f8e90084b4a52
LGXfufile <401040990@qq.com>
2022-05-24 20:33:13 +0800

    拆分header

---
+++ src/components/NewHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="xxx">
+    影视剧文案万能搜索工具
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewHeader"
+}
+</script>
+
+<style scoped>
+.xxx {
+  text-align: center;
+  font-weight: bold;
+  font-size: 26px;
+  color: black;
+
+}
+</style>
