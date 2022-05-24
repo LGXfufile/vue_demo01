6149f28ecd95e7aef6c475b5460b53291916285e
LGXfufile <401040990@qq.com>
2022-05-24 19:58:15 +0800

    增加MyFooter

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footStyle">
+    <span>Copyright by simba</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter"
+}
+</script>
+
+<style scoped>
+.footStyle{
+  text-align: center;
+  margin: 10px 0;
+}
+
+</style>
